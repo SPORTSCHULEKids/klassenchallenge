39b736e520811d239667781095dedfe655c30194
SPORTSCHULEKids <kids@sportschule.co.at>
2025-04-23 11:57:12 +0200

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import KlassenChallengeForm from './KlassenChallengeForm'
+import './style.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <KlassenChallengeForm />
+  </React.StrictMode>,
+)
