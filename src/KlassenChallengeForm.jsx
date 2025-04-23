58a2e5dff75093165cf553a9520aae5b8f4653f1
SPORTSCHULEKids <kids@sportschule.co.at>
2025-04-23 14:32:28 +0200

    Add files via upload

---
+++ src/KlassenChallengeForm.jsx
@@ -0,0 +1,1 @@
+export default function KlassenChallengeForm() { return <div>Buchungsformular</div>; }
---
+++ src/components/ui/select.jsx
@@ -0,0 +1,5 @@
+export const Select = ({ children }) => <div>{children}</div>;
+export const SelectTrigger = ({ children }) => <div>{children}</div>;
+export const SelectContent = ({ children }) => <div>{children}</div>;
+export const SelectItem = ({ children }) => <div>{children}</div>;
+export const SelectValue = ({ placeholder }) => <span>{placeholder}</span>;
